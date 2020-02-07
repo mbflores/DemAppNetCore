8c2f79765a983b67843b10f54c7c2fbb0aae1e85
Mel Bryan Flores <melbryanflores316@hotmail.com>
2020-02-07 22:19:35 +0800

    Finished demo branch

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemAppNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemAppNetCore.Controllers
+{
+    [Route("[controller]")]
+    public class PersonsController : Controller
+    {
+        private readonly PersonDbContext _personDbContext;
+
+        public PersonsController(PersonDbContext personDbContext)
+        {
+            _personDbContext = personDbContext;
+        }
+        public IActionResult Index()
+        {
+            var persons = _personDbContext.Persons.ToList();
+            return View(persons);
+        }
+
+        [HttpGet("Save")]
+        public IActionResult Save()
+        {
+            var random = new Random();
+            var randomNumber = random.Next(0,100);
+            var person = new Person
+            {
+                FirstName = "FirstName" + randomNumber,
+                LastName = "LastName" + randomNumber,
+                Address = "Address" + randomNumber
+            };
+            _personDbContext.Persons.Add(person);
+            _personDbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        //update
+
+        [HttpGet("update/{id}")]
+        public IActionResult UpdatePerson(int id)
+        {
+            var personInDb = _personDbContext.Persons.FirstOrDefault(x => x.Id == id);
+            if (personInDb == null)
+            {
+                return BadRequest();
+            }
+
+            personInDb.FirstName += "Updated";
+            personInDb.LastName += "Updated";
+            personInDb.Address += "Updated";
+            _personDbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        //delete
+
+        [HttpGet("delete/{id}")]
+        public IActionResult DeletePerson(int id)
+        {
+            var personInDb = _personDbContext.Persons.FirstOrDefault(x => x.Id == id);
+            if (personInDb == null)
+            {
+                return BadRequest();
+            }
+
+            _personDbContext.Persons.Remove(personInDb);
+            _personDbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemAppNetCore.Models
+{
+    public class Person
+    {
+        //id, firstName,LastName, Address
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+    }
+
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Person> Persons { get; set; }
+        //constructor, dbset
+    }
+}
