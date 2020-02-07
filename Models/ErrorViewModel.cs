4f2d7855896c2201544ea20874180f856346b1fa
Mel Bryan Flores <mel.flores@bcstechnology.com.au>
2020-02-07 20:45:39 +0800

    [MBF] - Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DemAppNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemAppNetCore.Models
+{
+    public class Person
+    {
+      
+    }
+
+    public class PersonDbContext : DbContext
+    {
+        public PersonDbContext(DbContextOptions<PersonDbContext> options):base(options)
+        {
+                
+        }
+
+        public DbSet<Person> People{ get; set; }
+    }
+}
